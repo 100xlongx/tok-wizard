f22b57bf5ce5460d77c76bc52b3f0e3fe754f9f1
100xlong <100xlong@matricalabs.io>
2024-02-02 19:24:06 -0500

    first commit

---
+++ src/server/api/root.ts
@@ -0,0 +1,14 @@
+import { postRouter } from "@tok-wizard/server/api/routers/post";
+import { createTRPCRouter } from "@tok-wizard/server/api/trpc";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  post: postRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
