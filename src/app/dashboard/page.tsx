97a43fae8c76867f9402a2a9a1face35bd11a2c4
100xlong <100xlong@100xlongs-MacBook-Pro.local>
2024-02-03 15:47:14 -0500

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-800 to-black text-white">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+                {/* Place your components from Shad's UI here */}
+            </div>
+        </div>
+    );
 }
