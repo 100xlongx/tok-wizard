a7edea8ce4955ef60067a58ba85f9e7f2688aa2d
100xlong <100xlong@matricalabs.io>
2024-02-02 20:55:32 -0500

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@tok-wizard/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "@tok-wizard/trpc/react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,10 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <TRPCReactProvider>
+        <html lang="en">
+          <body className={`font-sans ${inter.variable}`}>
+            {children}
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </ClerkProvider>
   );
 }
