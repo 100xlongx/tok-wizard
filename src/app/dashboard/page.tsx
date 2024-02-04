fef1f0ed888ee091a5a25a416a581ed33e37af9b
100xlong <100xlong@matricalabs.io>
2024-02-03 20:36:00 -0500

    added shad + aliases

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { api } from "@tok-wizard/trpc/server";
+import { Button } from '@components/shadcn/ui/button';
 
 export default async function Page() {
     // const messages = await api.message.getLatest.query();
@@ -11,6 +12,7 @@ export default async function Page() {
         <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-800 to-black text-white">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                 {/* Place your components from Shad's UI here */}
+                <Button>Click me</Button>
             </div>
         </div>
     );
